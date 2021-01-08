044c49f01f480f1324f1a87c71d131b5c5b4863b
Renan-Bodom <renan1211@hotmail.com>
2021-01-08 18:44:19 -0300

    Função para o botão

--- home/static/js/scripts.js
+++ home/static/js/scripts.js
@@ -1,1 +1,7 @@
 console.log('JavaScripts funcionando!');
+
+
+function btnAqui(){
+    var alter = document.getElementById("DIVContador");
+    alter.innerHTML = "Parabéns, você apertou o botão!";
+}
