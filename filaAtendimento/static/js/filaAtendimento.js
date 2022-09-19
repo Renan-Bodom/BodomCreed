ba6e4dafded8aeb9cefbc450e8afa12e84fddd2e
Renan-Bodom <renan1211@hotmail.com>
2022-09-19 19:19:14 -0300

    Nome arquivo alterado

--- filaAtendimento/static/js/filaAtendimento.js
+++ filaAtendimento/static/js/filaAtendimento.js
@@ -5,5 +5,5 @@ var contAqui = 0;
 function btnAqui(){
     var alter = document.getElementById("DIVContador");
     contAqui++;
-    alter.innerHTML = "Parabéns, você apertou o botão " + contAqui + " vezes!";
+    alter.innerHTML = "Parabéns, você apertou o botão " + contAqui + " vezes!!";
 }
