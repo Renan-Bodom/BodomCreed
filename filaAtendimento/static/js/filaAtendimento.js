98b4def67f35cbcc5d73f447e60f45795fa7f345
Renan <renan1211@hotmail.com>
2022-10-05 00:52:07 -0300

    Dashboard para a fila

--- filaAtendimento/static/js/filaAtendimento.js
+++ filaAtendimento/static/js/filaAtendimento.js
@@ -1,16 +1,16 @@
 console.log('JavaScripts funcionando!!');
 
-
+/* Teste ao pressionar botão */
 var contAqui = 0;
 function btnAqui(){
     var alter = document.getElementById("DIVContador");
     contAqui++;
-    alter.innerHTML = "Parabéns, você apertou o botão " + contAqui + " vezes!!";
+    alter.innerHTML = "JavaScript funcionando, botão pressionado " + contAqui + " vezes!!";
 }
-
 
 
 /* Hora */
 var dataAgora = new Date;
 var horaAgora = document.getElementById("horaAgora");
 horaAgora.innerHTML = "Atualizado em: " + dataAgora.getHours() + ':' + dataAgora.getMinutes();
+
